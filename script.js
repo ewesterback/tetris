147878d7bab575f25f63115b3851825f0cc67852
Emily Westerback <emilywesterback2@gmail.com>
2021-05-10 11:25:02 -0700

    fill square and clear square

--- script.js
+++ script.js
@@ -5,8 +5,9 @@ console.log('hi')
 
 const gameGrid = document.querySelector('.board-container')
 const BODY = document.querySelector('body')
+const masterGameArr = new Array(200)
 
-// ///////////////////////////////
+// ///////////////////////////////////
 // Functions
 // ///////////////////////////////////
 
@@ -18,41 +19,86 @@ const range = (begin, end, step) =>
   Array.from({ length: (end - begin) / step + 1 }, (_, i) => begin + i * step)
 
 // ---------------------------------
-class ElementFactory {
-  createElement(type, props) {
-    let el = document.createElement(type)
-    for (let key in props) {
-      el[key] = props[key]
+// setUpPage -may need to deconstruct from function
+// Sets up game grid
+// I/O: none
+// -------------------------------
+const setUpPage = () => {
+  class ElementFactory {
+    createElement(type, props) {
+      let el = document.createElement(type)
+      for (let key in props) {
+        el[key] = props[key]
+      }
+      return el
     }
-    return el
   }
+
+  class TetrisSquare extends ElementFactory {
+    constructor(numPos) {
+      super()
+      this.numPos = numPos
+    }
+    createGridSquare() {
+      //console.log('x')
+      return this.createElement('div', {
+        className: 'grid-square',
+        id: `pos${this.numPos}`,
+        innerHTML: `<p>${this.numPos}</p>`
+      })
+    }
+  }
+
+  const gridArray = range(0, 199, 1)
+
+  gridArray.forEach((square) => {
+    let mySquare = new TetrisSquare(square).createGridSquare()
+    gameGrid.appendChild(mySquare)
+  })
+}
+// ----------------------------------
+// fillSquare
+// Descr: HTML side to color squares
+// I/O:
+//    input: posArr - array with positions (numeric)  - required
+//          colorClass - class to denote color - optional
+// ----------------------------------
+const fillSquare = (posArr, colorClass = 'filled') => {
+  posArr.forEach((pos) => {
+    document.getElementById(`pos${pos}`).className = `grid-square ${colorClass}`
+  })
+}
+// ----------------------------------
+// clearSquare
+// Descr: HTML side to clear squares
+// I/O:
+//    input: posArr - array with positions (numeric)  - required
+// ----------------------------------
+const clearSquare = (posArr) => {
+  posArr.forEach((pos) => {
+    document.getElementById(`pos${pos}`).className = `grid-square`
+  })
 }
 
-class TetrisSquare extends ElementFactory {
-  constructor(numPos) {
-    super()
-    this.numPos = numPos
-  }
-  createGridSquare() {
-    //console.log('x')
-    return this.createElement('div', {
-      className: 'grid-square',
-      id: `pos${this.numPos}`,
-      innerHTML: `<p>${this.numPos}</p>`
-    })
-  }
-}
-
-const gridArray = range(1, 200, 1)
-console.log(gridArray)
-
-gridArray.forEach((square) => {
-  //gameGrid.appendChild(new TetrisSquare(square).createGridSquare())
-  let mySquare = new TetrisSquare(square).createGridSquare()
-  //console.log(mySquare)
-  gameGrid.appendChild(mySquare)
-})
-// ---------------------------
-console.log(gameGrid)
-
+// //////////////////////////////////////
+// main code
+// //////////////////////////////////
+setUpPage()
+fillSquare([0, 1, 10, 11])
 console.log('bye')
+
+// document.addEventListener('keypress', function(e) {
+//   var code = e.which || e.keyCode;
+//   if (code == '38') {
+//       // Up
+//   }
+//   else if (code == '40') {
+//       // Down
+//   }
+//   else if (code == '37') {
+//      // Left
+//   }
+//   else if (code == '39') {
+//      // Right
+//   }
+// })
